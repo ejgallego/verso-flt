765d68033822f773e503bfb118c19215d31806be
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-07-16 00:39:03 +0200

    chore: refresh FLT dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require FLT from "./FLT"
 require VersoBlueprint from git "https://github.com/leanprover/verso-blueprint.git" @ "v4.32.0"
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "c0cbc05a6f61999c6e6e8afdcbbe0b3daff95a27"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "a3364faec42918fcd84a03a255b50570129f9ead"
 
 package FLTBlueprint where
   precompileModules := false
