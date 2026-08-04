cff433384503cb60a1083bbf6101a0d0de75b353
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-04 18:33:50 +0200

    chore: refresh reference Blueprint for v4.32

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require FLT from "./FLT"
 require VersoBlueprint from git "https://github.com/leanprover/verso-blueprint.git" @ "v4.32.0"
-require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "a3364faec42918fcd84a03a255b50570129f9ead"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "249c48c2c2b5d7ebf8e765c30ce13b680e99cf04"
 
 package FLTBlueprint where
   precompileModules := false
